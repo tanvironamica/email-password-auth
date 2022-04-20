08b3466dfc84c2b7f04df3fe9fc472141e59565d
tonnysanjidaakter <tonnysanjidaakter@gmail.com>
2022-04-20 22:03:18 +0600

    react email password auth commit

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzTPkmHhPW1wFhHN_NqPVc9KPVJOPKTYs",
+  authDomain: "email-password-auth-5a8f7.firebaseapp.com",
+  projectId: "email-password-auth-5a8f7",
+  storageBucket: "email-password-auth-5a8f7.appspot.com",
+  messagingSenderId: "533345156413",
+  appId: "1:533345156413:web:0ddd6ec8bbfcaf02eb9954"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
